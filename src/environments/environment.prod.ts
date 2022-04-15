3e8e9a151217c096804dfce41c5fac9a60f5a1db
Edu58 <edumuriithi58@gmail.com>
2022-04-15 11:07:45 +0300

    adds boilerplate

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  Authorization: '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'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Authorization: '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'
 };
 
 /*
