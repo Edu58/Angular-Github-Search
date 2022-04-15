b121e8990cfbc66db1294c239e7265178a279b24
Edu58 <edumuriithi58@gmail.com>
2022-04-15 14:22:16 +0300

    adds searchuser method in get-user-info service

--- src/app/Services/get-repo-info.service.ts
+++ src/app/Services/get-repo-info.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetRepoInfoService {
 
-  constructor() { }
+  baseUrl: string = 'https://api.github.com/search/users?q=' 
+  options: Object = {
+    Authorization: environment.Authorization
+  }
+
+  constructor ( private http: HttpClient ) { }
+  
+  searchUser (user: string): Observable<Object> {
+    return this.http.get(this.baseUrl + user, this.options)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { CardGridComponent } from './Components/card-grid/card-grid.component';
 import { CardComponent } from './Components/card/card.component';
+import { GetRepoInfoService } from './Services/get-repo-info.service';
+import { GetUserInfoService } from './Services/get-user-info.service';
 
 @NgModule({
   declarations: [
@@ -26,6 +29,7 @@ import { CardComponent } from './Components/card/card.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     FontAwesomeModule,
     MatToolbarModule,
     MatTabsModule,
@@ -34,7 +38,7 @@ import { CardComponent } from './Components/card/card.component';
     MatButtonModule,
     FormsModule
   ],
-  providers: [],
+  providers: [GetRepoInfoService,GetUserInfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
