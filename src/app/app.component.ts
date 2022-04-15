e4054b13c9476131b0ff497ffeff91094e71930b
Edu58 <edumuriithi58@gmail.com>
2022-04-15 12:31:50 +0300

    adds navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faSearchengin, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Github-Search';
+  searchIcon: IconDefinition = faSearchengin
+  homeIcon: IconDefinition =faHouse
 }
