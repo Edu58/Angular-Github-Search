95e5252a1485cbb09f22864cb3f7349bc0b0347a
Edu58 <edumuriithi58@gmail.com>
2022-04-17 11:06:48 +0300

    changes serchRepo service observable to promise

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -3,24 +3,30 @@ import { NgForm, NgModel } from '@angular/forms';
 import { GetRepoInfoService } from 'src/app/Services/get-repo-info.service';
 import { GetUserInfoService } from 'src/app/Services/get-user-info.service';
 
-@Component({
+@Component( {
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
-})
+  styleUrls: [ './search.component.css' ]
+} )
 export class SearchComponent implements OnInit {
 
-  constructor(private userApi: GetUserInfoService, private repoApi: GetRepoInfoService) { }
+  results: any
 
-  ngOnInit(): void {
+  constructor ( private userApi: GetUserInfoService, private repoApi: GetRepoInfoService ) { }
+
+  ngOnInit (): void {
   }
 
   getTheUser ( form: NgForm ) {
-    this.userApi.searchUser(form.value.username).subscribe(data => console.log(data))
+    this.userApi.searchUser( form.value.username ).subscribe( data => console.log( data ) )
   }
 
-  getRepo (form: NgForm) {
-    this.repoApi.searchRepo( form.value.repo )
-    }
-  
+  getRepo ( form: NgForm ) {
+    this.repoApi.searchRepo( form.value.repo ).then(
+      ( results: any ) => {
+        this.results = this.repoApi.repoResults
+        console.log(this.results)
+      }
+    )
+  }
 }
--- src/app/Services/get-repo-info.service.ts
+++ src/app/Services/get-repo-info.service.ts
@@ -13,42 +13,47 @@ export class GetRepoInfoService {
     Authorization: environment.Authorization
   }
 
-  results: Array<Repomodel> = []
+  repoResults: Array<Repomodel> = []
 
   constructor ( private api: HttpClient ) { }
   
 
-  searchRepo ( repo: string ) {
-    const promise: Promise<Object | undefined> = this.api.get( this.baseUrl + repo, this.options ).toPromise()
-    
-    promise.then( async ( data: any ) => {
-      await data.items.map( ( obj: any ) => {
-        let owner: string = obj.owner.login
-        let description!: string
-        let repoUrl: string = obj.html_url
-        let size: number = obj.size
-        let license!: string
-        let language: string = obj.language
-        let allowForking: boolean = obj.allow_forking
-        let visibility: string = obj.visibility
-        let forks: number = obj.forks
+  searchRepo ( repo: string ): Promise<void> {
+    return new Promise<void>( ( resolve, reject ) => {
+      this.api.get( this.baseUrl + repo, this.options ).toPromise()
+        .then( ( data: any ) => {
+          data.items.map( ( obj: any ) => {
+            let owner: string = obj.owner.login
+            let description!: string
+            let repoUrl: string = obj.html_url
+            let size: number = obj.size
+            let license!: string
+            let language: string = obj.language
+            let allowForking: boolean = obj.allow_forking
+            let visibility: string = obj.visibility
+            let forks: number = obj.forks
 
-        if(obj.license && obj.license.name) {
-          license = obj.license.name
-        }
+            if ( obj.license && obj.license.name )
+            {
+              license = obj.license.name
+            }
 
-        if(obj.description) {
-          description = obj.description
-        }
+            if ( obj.description )
+            {
+              description = obj.description
+            }
 
-        this.results.push(new Repomodel( owner, repoUrl, description, size, language, license, allowForking, visibility, forks ))
-      } )
-
-      return this.results
-    } )
-      
-      .catch( ( err: string ) => {
-        console.error( err )
-      } )
+            this.repoResults.push( new Repomodel( owner, repoUrl, description, size, language, license, allowForking, visibility, forks ) )
+        
+            resolve()
+        
+          } ), ( err: string ) => {
+            console.error( err )
+            reject()
+          } 
+        } )
+        
+    }
+    )
   }
 }
